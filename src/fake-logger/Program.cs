10e63b9b806df49aef465de2b9adc5aceceaa3b7
Marcel Hoyer <m.hoyer@cid.com>
2019-11-06 19:13:17 +0100

    add counter to fake-logger to catch misses on startup

--- src/fake-logger/Program.cs
+++ src/fake-logger/Program.cs
@@ -13,6 +13,7 @@ namespace FakeLogger
         static Random rnd = new Random();
         static Logger log;
         static Counter logMetrics;
+        static ulong globalLogCounter = 0;
 
         static void Err(string msgTmpl, params object[] args)
         {
@@ -39,23 +40,25 @@ namespace FakeLogger
             var db = (new []{ "Bla Blub EN", "Blub EN", "Foo Boo Zoo News DE", "Arrrrrrrhhh_DE" })[rnd.Next(0, 3)];
             var p = rnd.Next(0, 2300);
 
-            if (p == 0)    Err("{@scope} Something terrible went wrong {@thread}[{@db}]", "Database.Disk", "DatabaseOperationThread", db);
-            else if (p <= 10)   Dbg("{@scope} Commit {@ssv} done (took: 123ms) {@thread}[{@db}]", "Database.Disk", ssv, "DatabaseOperationThread", db);
-            else if (p <= 20)   Dbg("{@scope} Committing {@ssv}... {@thread}[{@db}]", "Database.Disk", ssv, "DatabaseOperationThread", db);
-            else if (p <= 30)   Dbg("{@scope} New snapshot version {@ssv} was built for database '{@db}', new document count {@docCount}, took XXXms {@thread}", "Database.Snapshot", ssv, db, docCount, "SnapshotWorker#25");
-            else if (p <= 40)   Dbg("{@scope} No changes for snapshot version {@ssv} of '{@db}', document count: {@docCount} {@thread}", "Database.Snapshot", ssv, db, docCount, "SnapshotWorker#25");
-            else if (p <= 70)   Dbg("{@scope} 1 snapshot creations were skipped for '{@db}'. {@thread}", "Database.Snapshot", db, "SnapshotWorker#25");
-            else if (p <= 160)  Inf("{@scope} Received LOG_ENTRIES (7 entries), scheduling now {@thread}", "InternalCommunication.Replication", "Replication thread");
-            else if (p <= 250)  Dbg("{@scope} Sending GetLogEntries(DataspaceID: 426f86bca105913b, Versions: {@ssv}) {@thread}", "InternalCommunication.Replication", ssv, "Replication thread");
-            else if (p <= 350)  Dbg("{@scope} 7 entries scheduled {@thread}", "InternalCommunication.Replication", "Replication thread");
-            else if (p <= 450)  Dbg("{@scope} No more work items for background snapshot creations. {@thread}", "Database.Snapshot", "SnapshotWorker#25");
-            else if (p <= 670)  Dbg("{@scope} Increased snapshot version for '{@db}' from {@ssvOld} to version {@ssvNew}, document count: {doccount} {@thread}", "Database.Snapshot", db, ssv, ssv+2, "SnapshotWorker#27");
-            else if (p <= 900)  Inf("{@scope} Snapshot version {@ssv} for '{@db}' has been disposed, took 0 ms. {@thread}", "Database.Snapshot", ssv, db, "SnapshotWorker#25");
-            else if (p <= 1140) Dbg("{@scope} 1kB have been written to position 99 in file '~\\SomeWhere\\Over\\The\\Rainbow\\Index\\DataLog\\271e16.datalog' (took 0ms) {@thread}", "Dataspace.DataLog", "Replication thread");
-            else if (p <= 1410) Dbg("{@scope} Increasing version to {@ssv} without actual changes. {@thread}[{@db}]", "Database.Disk", ssv, "DatabaseOperationThread", db);
-            else if (p <= 1690) Dbg("{@scope} Data log entry 393805899 of file 271e16.datalog marked as executed (with success). {@thread}[{@db}]", "Dataspace.DataLog", "DatabaseOperationThread", db);
-            else if (p <= 1970) Inf("{@scope} Snapshot update scheduled for version {@ssv} {@thread}[{@db}]", "Database.Snapshot", ssv, "DatabaseOperationThread", db);
-            else           Dbg("{@scope} Recovered operation (UpdateDocumentsOperationData, Guid: f6ceb5df-03d2-41bd-a69f-8e70a23e83cc, Version: 393805899) {@thread}", "InternalCommunication.Replication", "Replication thread");
+            if (p == 0)         Err("{@counter} {@scope} Something terrible went wrong {@thread}[{@db}]",                                                                                       globalLogCounter, "Database.Disk", "DatabaseOperationThread", db);
+            else if (p <= 10)   Dbg("{@counter} {@scope} Commit {@ssv} done (took: 123ms) {@thread}[{@db}]",                                                                                    globalLogCounter, "Database.Disk", ssv, "DatabaseOperationThread", db);
+            else if (p <= 20)   Dbg("{@counter} {@scope} Committing {@ssv}... {@thread}[{@db}]",                                                                                                globalLogCounter, "Database.Disk", ssv, "DatabaseOperationThread", db);
+            else if (p <= 30)   Dbg("{@counter} {@scope} New snapshot version {@ssv} was built for database '{@db}', new document count {@docCount}, took XXXms {@thread}",                     globalLogCounter, "Database.Snapshot", ssv, db, docCount, "SnapshotWorker#25");
+            else if (p <= 40)   Dbg("{@counter} {@scope} No changes for snapshot version {@ssv} of '{@db}', document count: {@docCount} {@thread}",                                             globalLogCounter, "Database.Snapshot", ssv, db, docCount, "SnapshotWorker#25");
+            else if (p <= 70)   Dbg("{@counter} {@scope} 1 snapshot creations were skipped for '{@db}'. {@thread}",                                                                             globalLogCounter, "Database.Snapshot", db, "SnapshotWorker#25");
+            else if (p <= 160)  Inf("{@counter} {@scope} Received LOG_ENTRIES (7 entries), scheduling now {@thread}",                                                                           globalLogCounter, "InternalCommunication.Replication", "Replication thread");
+            else if (p <= 250)  Dbg("{@counter} {@scope} Sending GetLogEntries(DataspaceID: 426f86bca105913b, Versions: {@ssv}) {@thread}",                                                     globalLogCounter, "InternalCommunication.Replication", ssv, "Replication thread");
+            else if (p <= 350)  Dbg("{@counter} {@scope} 7 entries scheduled {@thread}",                                                                                                        globalLogCounter, "InternalCommunication.Replication", "Replication thread");
+            else if (p <= 450)  Dbg("{@counter} {@scope} No more work items for background snapshot creations. {@thread}",                                                                      globalLogCounter, "Database.Snapshot", "SnapshotWorker#25");
+            else if (p <= 670)  Dbg("{@counter} {@scope} Increased snapshot version for '{@db}' from {@ssvOld} to version {@ssvNew}, document count: {doccount} {@thread}",                     globalLogCounter, "Database.Snapshot", db, ssv, ssv+2, "SnapshotWorker#27");
+            else if (p <= 900)  Inf("{@counter} {@scope} Snapshot version {@ssv} for '{@db}' has been disposed, took 0 ms. {@thread}",                                                          globalLogCounter, "Database.Snapshot", ssv, db, "SnapshotWorker#25");
+            else if (p <= 1140) Dbg("{@counter} {@scope} 1kB have been written to position 99 in file '~\\SomeWhere\\Over\\The\\Rainbow\\Index\\DataLog\\271e16.datalog' (took 0ms) {@thread}", globalLogCounter, "Dataspace.DataLog", "Replication thread");
+            else if (p <= 1410) Dbg("{@counter} {@scope} Increasing version to {@ssv} without actual changes. {@thread}[{@db}]",                                                                globalLogCounter, "Database.Disk", ssv, "DatabaseOperationThread", db);
+            else if (p <= 1690) Dbg("{@counter} {@scope} Data log entry 393805899 of file 271e16.datalog marked as executed (with success). {@thread}[{@db}]",                                  globalLogCounter, "Dataspace.DataLog", "DatabaseOperationThread", db);
+            else if (p <= 1970) Inf("{@counter} {@scope} Snapshot update scheduled for version {@ssv} {@thread}[{@db}]",                                                                        globalLogCounter, "Database.Snapshot", ssv, "DatabaseOperationThread", db);
+            else                Dbg("{@counter} {@scope} Recovered operation (UpdateDocumentsOperationData, Guid: f6ceb5df-03d2-41bd-a69f-8e70a23e83cc, Version: 393805899) {@thread}",         globalLogCounter, "InternalCommunication.Replication", "Replication thread");
+
+            globalLogCounter++;
         }
 
         static void Main(string[] args)
